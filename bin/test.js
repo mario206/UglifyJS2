cd6b55cabf791ae795217342c0ef6a3f7021849d
hellogdut <hellogdut@users.noreply.github.com>
2018-12-07 21:35:44 +0800

    1
    1

---
+++ bin/test.js
@@ -0,0 +1,9 @@
+function main() {
+    var a = Math.random();
+    var b = Math.random();
+    var c = a + b;
+    a ? console.log(1) : console.log(2);
+    console.log(!!c);
+    var kkkaf = Win();
+    console.log(kkkaf);
+}
